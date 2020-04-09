16da023d82e4d17994840b7a88491ea4db256e28
ghorbani <aali361@gmail.com>
2020-04-09 20:59:59 +0430

    Adding cross db json field

--- agency/models.py
+++ agency/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-from django.contrib.postgres.fields import JSONField
+# from django.contrib.postgres.fields import JSONField
+import jsonfield
+
 
 # Create your models here.
 class Agency(models.Model):
     name = models.CharField(max_length=20, null=False, unique=True)
     website = models.CharField(max_length=100, null=False)
-    crawl_headers = JSONField(null=True)
+    crawl_headers = jsonfield.JSONField(null=True, blank=True, default={})
     status = models.BooleanField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -23,5 +25,5 @@ class AgencyPageStructure(models.Model):
     agency = models.ForeignKey(Agency, on_delete=models.DO_NOTHING)
     page = models.CharField(max_length=300, null=False)
     crawl_interval = models.IntegerField(default=12)
-    news_links_structure = JSONField()
-    news_meta_structure = JSONField()
+    news_links_structure = jsonfield.JSONField()
+    news_meta_structure = jsonfield.JSONField()
