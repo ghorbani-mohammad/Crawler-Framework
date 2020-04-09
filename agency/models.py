03254edc7cdc10909625e5553218b27bfb12068c
ghorbani <aali361@gmail.com>
2020-04-09 15:26:18 +0430

    Adding AgencyPageStructure & CrawlReport

--- agency/models.py
+++ agency/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
 class Agency(models.Model):
@@ -6,9 +7,16 @@ class Agency(models.Model):
     website = models.TextField(null=False)
     crawl_interval = models.IntegerField(default=12)
     status = models.BooleanField(default=1)
-    last_crawl = models.DateTimeField()
-    last_crawl_duration = models.TextField()
-    last_crawl_status = models.BooleanField()
-    number_of_crawls = models.IntegerField()
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class CrawlReport(models.Model):
+    agency = models.ForeignKey(Agency, on_delete=models.DO_NOTHING)
+    last_crawl = models.DateTimeField(null=True)
+    last_crawl_duration = models.DateTimeField(null=True)
+    last_crawl_status = models.BooleanField(null=True)
+
+class AgencyPageStructure(models.Model):
+    agency = models.ForeignKey(Agency, on_delete=models.DO_NOTHING)
+    page = models.TextField(null=False)
+    structure = JSONField()
--- agency/serializer.py
+++ agency/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from agency.models import Agency
+from agency.models import Agency, CrawlReport, AgencyPageStructure
 
 
 class AgencySerializer(serializers.ModelSerializer):
@@ -10,10 +10,26 @@ class AgencySerializer(serializers.ModelSerializer):
                     'website',
                     'crawl_interval',
                     'status',
+                    'number_of_crawls',
+                    'created_at',
+                    'updated_at',
+        ]
+
+class CrawlReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CrawlReport
+        fields = [
+                    'agency',
                     'last_crawl',
                     'last_crawl_duration',
                     'last_crawl_status',
-                    'number_of_crawls',
-                    'created',
-                    'updated',
+        ]
+
+class AgencyPageStructureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AgencyPageStructure
+        fields = [
+                    'agency',
+                    'page',
+                    'structure',
         ]
