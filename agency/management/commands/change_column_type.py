784acc1efbcc56cec24c2be30c9cba1fd89ce891
ghorbani <aali361@gmail.com>
2020-04-17 15:18:59 +0430

    Adding change_column_type command && changes on other commands

---
+++ agency/management/commands/change_column_type.py
@@ -0,0 +1,8 @@
+from django.db import connection
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = 'Change type of news_meta_structure from jsonb to json'
+    def handle(self, *args, **options):
+        cursor = connection.cursor()
+        cursor.execute('''ALTER TABLE agency_agencypagestructure ALTER COLUMN news_meta_structure TYPE json''')
--- agency/management/commands/init_admin_user.py
+++ agency/management/commands/init_admin_user.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
-    help = 'Crate a superuser, and allow password to be provided by Mohammad Ghorbani'
+    help = 'Crate a superuser user=army and pass=army'
     def handle(self, *args, **options):
         User = get_user_model()
         User.objects.create_superuser('army', password='army')
