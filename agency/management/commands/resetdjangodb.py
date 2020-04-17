c94debc1f7726fbf066337827fdeb1f5a27cd255
ghorbani <aali361@gmail.com>
2020-04-17 15:06:22 +0430

    Add command resetdjangodb

---
+++ agency/management/commands/resetdjangodb.py
@@ -0,0 +1,8 @@
+from django.db import connection
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = 'Drop django tables and ageny, pagestructure, agencycrawlreport tables by Mohammad Ghorbani'
+    def handle(self, *args, **options):
+        cursor = connection.cursor()
+        cursor.execute('''drop table auth_group, auth_group_permissions, auth_permission, auth_user, auth_user_groups, auth_user_user_permissions, agency_agency, agency_agencypagestructure, agency_crawlreport, django_admin_log, django_content_type, django_migrations, django_session CASCADE''')
