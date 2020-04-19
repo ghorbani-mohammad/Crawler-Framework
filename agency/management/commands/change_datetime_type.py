4ca1cf98b138640e8c9c651256273dad2b22948b
ghorbani <aali361@gmail.com>
2020-04-19 07:03:59 +0430

    Add command cange column types from with timezone to without timezone & command django-clear-cache

---
+++ agency/management/commands/change_datetime_type.py
@@ -0,0 +1,12 @@
+from django.db import connection
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = 'Change type of datetime column from with timezone to without timezone'
+    def handle(self, *args, **options):
+        cursor = connection.cursor()
+        cursor.execute('''ALTER TABLE agency_agency ALTER COLUMN created_at TYPE "timestamp"''')
+        cursor.execute('''ALTER TABLE agency_agencypagestructure ALTER COLUMN created_at TYPE "timestamp"''')
+        cursor.execute('''ALTER TABLE agency_agency ALTER COLUMN updated_at  TYPE "timestamp"''')
+        cursor.execute('''ALTER TABLE agency_agencypagestructure ALTER COLUMN updated_at     TYPE "timestamp"''')
+        cursor.execute('''ALTER TABLE agency_agencypagestructure ALTER COLUMN last_crawl TYPE "timestamp"''')
