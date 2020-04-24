0d4310b6980a55b14562ee7e60cb741359242133
ghorbani <aali361@gmail.com>
2020-04-24 17:46:25 +0430

    Adding model tables to admin

--- agency/admin.py
+++ agency/admin.py
@@ -1,8 +1,32 @@
+import datetime as dt
 from django.contrib import admin
-
-# Register your models here.
 from agency.models import Agency, AgencyPageStructure, CrawlReport
 
-admin.site.register(Agency)
-admin.site.register(AgencyPageStructure)
-# admin.site.register(CrawlReport)
+
+@admin.register(CrawlReport)
+class CrawlAdmin(admin.ModelAdmin):
+    list_display = ('id', 'agency', 'page', 'fetched_links', 'new_links', 'started_at', 'duration', 'status')
+    list_per_page = 10
+
+    def agency(self, obj):
+        return obj.page.agency.name
+
+    def started_at(self, obj):
+        return obj.created_at
+
+    def duration(self, obj):
+        x = round((obj.updated_at - obj.created_at).total_seconds())
+        return "{} sec".format(x)
+
+
+@admin.register(Agency)
+class AgencyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'country', 'website')
+
+
+@admin.register(AgencyPageStructure)
+class AgencyPageStructureAdmin(admin.ModelAdmin):
+    list_display = ('id', 'agency', 'url', 'crawl_interval', 'last_crawl')
+
+    def agency(self, obj):
+        return obj.page.agency.name
