5a1fea7bea5c9e7e6793a5356e5478da96251f86
ghorbani <aali361@gmail.com>
2020-11-30 01:28:40 +0330

    make lock false in initialization

--- app/wsgi.py
+++ app/wsgi.py
@@ -14,3 +14,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
+
+
+from agency.models import AgencyPageStructure, CrawlReport
+print('********* {} update: lock=False *****'.format(AgencyPageStructure.objects.filter(lock=True).update(lock=False)))
+print('********* {} update: status=faild *****'.format(CrawlReport.objects.filter(status='pending').update(status='failed')))
