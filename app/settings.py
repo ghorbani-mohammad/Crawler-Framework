94a02dc3413fd87623429b2a0c6bea056433da66
Mohammad <aali361@gmail.com>
2021-10-24 12:49:01 +0330

    feat: change env package

--- app/settings.py
+++ app/settings.py
@@ -1,10 +1,8 @@
 import os
+from envparse import env
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-import environ
-env = environ.Env()
-environ.Env.read_env()
 
 SECRET_KEY = env('SECRET_KEY') # SECURITY WARNING: keep the secret key used in production secret!
 DEBUG = env('DEBUG') # SECURITY WARNING: don't run with debug turned on in production!
