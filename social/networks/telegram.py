b39397d54667bfa145f055c453704e1dc29d1b84
Mohammad <aali361@gmail.com>
2021-11-23 12:37:11 +0330

    feat: add network cralwers template class

---
+++ social/networks/telegram.py
@@ -0,0 +1,11 @@
+from .base import Network
+
+class Telegram(Network):
+    def login(self):
+        pass
+    def logout(self):
+        pass
+    def follow(self, page):
+        pass
+    def unfollow(self, page):
+        pass
