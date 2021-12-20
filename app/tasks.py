c42c2771bbe22f7b92918ff10dc7d758dfdbc8f4
Mohammad <aali361@gmail.com>
2021-12-21 02:13:28 +0330

    feat: rm fetch alexa rank

--- app/tasks.py
+++ app/tasks.py
@@ -155,42 +155,6 @@ def redis_exporter():
             redis_news.delete(key)
     redis_news.set("redis_exporter_lock", 0)
 
-
-@crawler.task(name="fetch_alexa_rank")
-def fetch_alexa_rank(agency_id, agency_url):
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--disable-dev-shm-usage")
-
-    driver = webdriver.Remote(
-        "http://crawler_chrome_browser:4444/wd/hub",
-        desired_capabilities=DesiredCapabilities.CHROME,
-        options=options,
-    )
-    driver.header_overrides = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) "
-        "AppleWebKit/537.11 (KHTML, like Gecko) "
-        "Chrome/23.0.1271.64 Safari/537.11",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
-        "Accept-Encoding": "none",
-        "Accept-Language": "en-US,en;q=0.8",
-        "Connection": "keep-alive",
-    }
-
-    driver.get("https://www.alexa.com/siteinfo/{}".format(agency_url))
-    doc = BeautifulSoup(driver.page_source, "html.parser")
-    global_rank = (
-        doc.find("div", {"class": "rankmini-rank"})
-        .text.replace("#", "")
-        .replace("\t", "")
-        .replace("\n", "")
-        .replace(",", "")
-    )
-    Agency.objects.filter(pk=agency_id).update(alexa_global_rank=global_rank)
-
-
 @crawler.task(name="remove_obsolete_reports")
 def remove_obsolete_reports():
     now = datetime.datetime.now()
