f72406eef27ac9559456957a4ecd3e33d356dde0
Mohammad <aali361@gmail.com>
2022-01-02 19:59:50 +0330

    feat: add log

--- crawler/agency/crawler_engine.py
+++ crawler/agency/crawler_engine.py
@@ -37,9 +37,13 @@ class CrawlerEngine:
         )
         self.header = header
         self.repetitive = repetitive
-        logger.info(f"CrawlerEngine initiated for page: {self.page}")
+        logger.info(
+            f"CrawlerEngine initiated for page: {self.page} with repetitive: {self.repetitive}"
+        )
         self.run()
-        logger.info(f"CrawlerEngine finished for page: {self.page}")
+        logger.info(
+            f"CrawlerEngine finished for page: {self.page} with repetitive: {self.repetitive}"
+        )
 
     def register_log(self, description, e, page, url):
         logger.error(traceback.format_exc())
