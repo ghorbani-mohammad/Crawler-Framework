0758c792914fd634d92f8a80c4ba26f4b99a3621
Mohammad <aali361@gmail.com>
2022-01-02 15:20:27 +0330

    feat: minor change

--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -86,10 +86,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 LANGUAGE_CODE = "en-us"
-TIME_ZONE = "Asia/Tehran"
 USE_I18N = True
-USE_L10N = False
+USE_L10N = True
 USE_TZ = True
+TIME_ZONE = "Asia/Tehran"
 
 # CELERY
 BROKER_URL = "redis://crawler_redis:6379"
