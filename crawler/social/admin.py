e4997b66a6d442a93f70a0d270388c988bef346f
Mohammad <aali361@gmail.com>
2022-01-21 11:22:55 +0330

    feat: add PublisherAdmin

--- crawler/social/admin.py
+++ crawler/social/admin.py
@@ -9,6 +9,10 @@ class NetworkAdmin(ReadOnlyAdminDateFields, admin.ModelAdmin):
     list_display = ("pk", "name", "url")
 
 
+@admin.register(models.Publisher)
+class PublisherAdmin(ReadOnlyAdminDateFields, admin.ModelAdmin):
+    list_display = ("pk", "username", "network", "is_channel", "created_at")
+
 @admin.register(models.Post)
 class PostAdmin(ReadOnlyAdminDateFields, admin.ModelAdmin):
     list_display = ("pk", "publisher", "created_at")
