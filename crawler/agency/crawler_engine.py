a5ebd5c21f3ec72f27bf070d45f796152402af31
Mohammad <aali361@gmail.com>
2022-02-07 04:34:08 +0330

    feat: change to firefox

--- crawler/agency/crawler_engine.py
+++ crawler/agency/crawler_engine.py
@@ -16,7 +16,7 @@ logger = get_task_logger(__name__)
 class CrawlerEngine:
     def __init__(self, page, repetitive=False, header=None):
         self.driver = webdriver.Remote(
-            "http://crawler_chrome:4444/wd/hub",
+            "http://crawler_firefox:4444/wd/hub",
             desired_capabilities=DesiredCapabilities.FIREFOX,
             options=utils.get_browser_options(),
         )
@@ -169,7 +169,7 @@ class CrawlerEngine:
 class CrawlerEngineV2:
     def __init__(self, header=None):
         self.driver = webdriver.Remote(
-            "http://crawler_chrome:4444/wd/hub",
+            "http://crawler_firefox:4444/wd/hub",
             desired_capabilities=DesiredCapabilities.FIREFOX,
             options=utils.get_browser_options(),
         )
