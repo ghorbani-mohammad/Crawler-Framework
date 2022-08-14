6335db3d87520f6c61b07d53a4309ef501a25646
Mohammad <aali361@gmail.com>
2022-08-14 23:44:36 +0430

    minor change

--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -3,8 +3,8 @@ from envparse import env
 from djangoeditorwidgets.config import *
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SECRET_KEY = env.str("SECRET_KEY")
 DEBUG = env.bool("DEBUG")
+SECRET_KEY = env.str("SECRET_KEY")
 BOT_API_KEY = env.str("BOT_API_KEY")
 ALLOWED_HOSTS = ["crawler.m-gh.com"]
 SERVER_IP = env.str("SERVER_IP")
