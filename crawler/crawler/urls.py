9508b7bfb5088935ae2bbaa514daf4339710034c
Mohammad <aali361@gmail.com>
2022-08-23 12:44:59 +0430

    rm unregister User,Group

--- crawler/crawler/urls.py
+++ crawler/crawler/urls.py
@@ -1,15 +1,11 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
-from django.contrib.auth.models import User
-from django.contrib.auth.models import Group
 
 urlpatterns = [
     path("secret-admin/", admin.site.urls),
     re_path("api/(?P<version>(v1|v2))/", include("agency.urls")),
 ]
 
-admin.site.unregister(User)
-admin.site.unregister(Group)
 admin.site.index_title = "Crawler"
 admin.site.site_title = "Crawler Django Admin"
 admin.site.site_header = "Crawler Django Administration Panel"
