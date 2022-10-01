56c88249737dc158a5cbe3577ff6e36daa0b16a5
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-01 19:45:47 +0330

    minor change

--- crawler/notification/utils.py
+++ crawler/notification/utils.py
@@ -3,11 +3,11 @@ import requests
 
 def telegram_bot_sendtext(token, chat_id, message):
     send_text = (
-        'https://api.telegram.org/bot'
+        "https://api.telegram.org/bot"
         + token
-        + '/sendMessage?chat_id='
+        + "/sendMessage?chat_id="
         + chat_id
-        + '&parse_mode=Markdown&text='
+        + "&parse_mode=Markdown&text="
         + message
     )
     response = requests.get(send_text)
