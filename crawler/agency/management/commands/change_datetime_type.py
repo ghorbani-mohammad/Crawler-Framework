f8dc87d10c31686667f303e972c5fb653aa72efe
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-01 19:42:04 +0330

    minor change

--- crawler/agency/management/commands/change_datetime_type.py
+++ crawler/agency/management/commands/change_datetime_type.py
@@ -1,15 +1,33 @@
 from django.db import connection
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
-    help = 'Change type of datetime column from with timezone to without timezone'
+    help = "Change type of datetime column from with timezone to without timezone"
+
     def handle(self, *args, **options):
         cursor = connection.cursor()
-        cursor.execute('''ALTER TABLE agency_agency ALTER COLUMN created_at TYPE "timestamp"''')
-        cursor.execute('''ALTER TABLE agency_crawlreport ALTER COLUMN created_at TYPE "timestamp"''')
-        cursor.execute('''ALTER TABLE agency_crawlreport ALTER COLUMN updated_at TYPE "timestamp"''')
-        cursor.execute('''ALTER TABLE agency_agencypagestructure ALTER COLUMN created_at TYPE "timestamp"''')
-        cursor.execute('''ALTER TABLE agency_agency ALTER COLUMN updated_at TYPE "timestamp"''')
-        cursor.execute('''ALTER TABLE agency_agency ALTER COLUMN deleted_at TYPE "timestamp"''')
-        cursor.execute('''ALTER TABLE agency_agencypagestructure ALTER COLUMN updated_at TYPE "timestamp"''')
-        cursor.execute('''ALTER TABLE agency_agencypagestructure ALTER COLUMN last_crawl TYPE "timestamp"''')
+        cursor.execute(
+            '''ALTER TABLE agency_agency ALTER COLUMN created_at TYPE "timestamp"'''
+        )
+        cursor.execute(
+            '''ALTER TABLE agency_crawlreport ALTER COLUMN created_at TYPE "timestamp"'''
+        )
+        cursor.execute(
+            '''ALTER TABLE agency_crawlreport ALTER COLUMN updated_at TYPE "timestamp"'''
+        )
+        cursor.execute(
+            '''ALTER TABLE agency_agencypagestructure ALTER COLUMN created_at TYPE "timestamp"'''
+        )
+        cursor.execute(
+            '''ALTER TABLE agency_agency ALTER COLUMN updated_at TYPE "timestamp"'''
+        )
+        cursor.execute(
+            '''ALTER TABLE agency_agency ALTER COLUMN deleted_at TYPE "timestamp"'''
+        )
+        cursor.execute(
+            '''ALTER TABLE agency_agencypagestructure ALTER COLUMN updated_at TYPE "timestamp"'''
+        )
+        cursor.execute(
+            '''ALTER TABLE agency_agencypagestructure ALTER COLUMN last_crawl TYPE "timestamp"'''
+        )
