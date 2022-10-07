3cce0cdcda42ae664740de00dbce059c55168dc9
mohammad <mohammad.ghorbani@hotmail.com>
2022-10-07 18:17:14 +0330

    minor change

--- crawler/agency/models.py
+++ crawler/agency/models.py
@@ -162,3 +162,6 @@ class Option(models.Model):
     value = models.CharField(max_length=70)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"({self.pk} - {self.key})"
