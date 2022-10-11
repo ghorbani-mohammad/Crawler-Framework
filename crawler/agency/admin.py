180761f20c026946a4aaf5286e50aec5466927e7
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-11 19:42:18 +0330

    minor change

--- crawler/agency/admin.py
+++ crawler/agency/admin.py
@@ -50,6 +50,7 @@ class ReportAdmin(admin.ModelAdmin):
         if obj.picture:
             url = "https://www.mo-ghorbani.ir/static/" + obj.picture.path.split("/")[-1]
             return format_html(f"<a href='{url}'>Link</a>")
+        return "-"
 
     image_tag.short_description = "Image"
 
