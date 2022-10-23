d4532b10468c58b46b4ff2a1311eb6552fe5e006
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-23 17:31:02 +0330

    minor change

--- crawler/agency/admin.py
+++ crawler/agency/admin.py
@@ -117,7 +117,11 @@ class PageAdmin(admin.ModelAdmin):
         ]
 
     form = PageAdminForm
-    list_filter = ("lock", "status", "agency")
+    list_filter = (
+        "lock",
+        "status",
+        "agency",
+    )
     list_editable = ("status", "crawl_interval")
     readonly_fields = ("created_at", "updated_at", "deleted_at", "last_crawl")
     list_display = (
