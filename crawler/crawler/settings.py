5ec466b3adb52159fe0eae3658999bfd1e6b0d6f
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-28 19:25:24 +0330

    improve readability

--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -89,10 +89,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-LANGUAGE_CODE = "en-us"
+USE_TZ = True
 USE_I18N = True
 USE_L10N = True
-USE_TZ = True
+LANGUAGE_CODE = "en-us"
 TIME_ZONE = "Asia/Tehran"
 
 # CELERY
