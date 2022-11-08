7ebf74087e0e771e2fbc17ed271d3a4a02d175a9
Mohammad <mohammad.ghorbani@hotmail.com>
2022-11-08 17:46:46 +0330

    minor change

--- crawler/agency/admin.py
+++ crawler/agency/admin.py
@@ -247,10 +247,7 @@ class LogAdmin(admin.ModelAdmin):
 @admin.register(DBLogEntry)
 class DBLogEntryAdmin(admin.ModelAdmin):
     list_filter = ("level",)
-    readonly_fields = (
-        "level",
-        "message",
-    )
+    readonly_fields = ("level", "message")
     list_display = (
         "pk",
         "level",
