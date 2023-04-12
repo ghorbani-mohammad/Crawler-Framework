37d903eb7c2831a96160610cc31f5b41fa0907f7
Mohammad <mohammad.ghorbani@hotmail.com>
2023-04-12 10:19:57 +0330

    style(reusable): solve linter issues

--- crawler/agency/management/commands/reset_django_tables.py
+++ crawler/agency/management/commands/reset_django_tables.py
@@ -3,10 +3,15 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = "Drop django tables and ageny, pagestructure, agencycrawlreport tables by Mohammad Ghorbani"
+    help = "Drop django tables and ageny, pagestructure, \
+        agencycrawlreport tables by Mohammad Ghorbani"
 
-    def handle(self, *args, **options):
+    def handle(self, *_args, **_options):
         cursor = connection.cursor()
         cursor.execute(
-            """drop table auth_group, auth_group_permissions, auth_permission, auth_user, auth_user_groups, auth_user_user_permissions, agency_agency, agency_agencypagestructure, agency_crawlreport, django_admin_log, django_content_type, django_migrations, django_session CASCADE"""
+            """drop table auth_group, auth_group_permissions, \
+                auth_permission, auth_user, auth_user_groups, auth_user_user_permissions, \
+                agency_agency, agency_agencypagestructure, \
+                agency_crawlreport, django_admin_log, django_content_type, \
+                django_migrations, django_session CASCADE"""
         )
