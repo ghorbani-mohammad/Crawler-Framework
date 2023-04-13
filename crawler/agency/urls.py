97002242c40b263a6b68628ad7bf900a3bdeb4cc
Mohammad <mohammad.ghorbani@hotmail.com>
2023-04-13 10:44:57 +0330

    style(agency): solve linter issues

--- crawler/agency/urls.py
+++ crawler/agency/urls.py
@@ -19,9 +19,9 @@ urlpatterns = [
     path("crawl/agency/<int:agency_id>/", views.crawl_agency),
     path("agency/<int:agency_id>/pages/", views.agency_pages),
     path("crawl/news/memory/list/", views.crawl_news_memory_list),
-    path("crawl/agency/active_all/", views.crawl_agency_activeAll),
+    path("crawl/agency/active_all/", views.crawl_agency_active_all),
     path("crawl/links/memory/list/", views.crawl_links_memory_list),
-    path("crawl/agency/disable_all/", views.crawl_agency_disableAll),
+    path("crawl/agency/disable_all/", views.crawl_agency_disable_all),
     path("crawl/memory/reset/<int:agency_id>/", views.crawl_agency_reset_memory),
     path(
         "crawl/agency/reset_and_crawl/<int:agency_id>/",
