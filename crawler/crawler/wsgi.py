9ef656d8f1549a9f78c1b161d8809d0051f34759
Mohammad <mohammad.ghorbani@hotmail.com>
2024-12-28 16:04:13 +0330

    feat: add clear_all_locks function to manage multiple Redis locks

--- crawler/crawler/wsgi.py
+++ crawler/crawler/wsgi.py
@@ -9,7 +9,7 @@ application = get_wsgi_application()
 try:
     import redis
     from agency.models import Page, Report
-    from agency.tasks import clear_redis_exporter_lock
+    from agency.tasks import clear_all_locks
 
     print(
         f"***** {Page.objects.filter(lock=True).update(lock=False)} update: lock=False *****"
@@ -18,7 +18,7 @@ try:
         f"***** {Report.objects.filter(status='pending').update(status='failed')} update: \
             status=failed *****"
     )
-    clear_redis_exporter_lock()
+    clear_all_locks()
     redis_news = redis.StrictRedis(host="crawler-redis", port=6379, db=0)
 except ImportError:
     pass
