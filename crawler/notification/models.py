981dfe2e64477fcd9e5692aada228d0bed26e627
Mohammad <mohammad.ghorbani@hotmail.com>
2024-12-29 17:02:55 +0330

    feat: add unique constraint on token and tag in FilteringToken model

--- crawler/notification/models.py
+++ crawler/notification/models.py
@@ -38,5 +38,8 @@ class FilteringToken(BaseModel):
     token = models.CharField(max_length=50)
     tag = models.ForeignKey(FilteringTag, on_delete=models.CASCADE)
 
+    class Meta:
+        unique_together = ["token", "tag"]
+
     def __str__(self):
         return f"({self.pk} - {self.token})"
