103355d3db0d9b8823accaed9cd208aafa0f829d
Mohammad <mohammad.ghorbani@hotmail.com>
2025-01-04 10:55:13 +0330

    feat: add OffTime model and integrate with Page for managing off-time periods

--- crawler/agency/models.py
+++ crawler/agency/models.py
@@ -102,6 +102,7 @@ class Page(BaseModel):
     filtering_tags = models.ManyToManyField(
         "notification.FilteringTag", related_name="pages", blank=True
     )
+    off_times = models.ManyToManyField("OffTime", related_name="pages", blank=True)
 
     def __str__(self):
         return f"({self.pk} - {self.name})"
@@ -120,6 +121,25 @@ class Page(BaseModel):
     def today_crawl_count(self):
         today = timezone.localtime().replace(hour=0)
         return self.report.filter(created_at__gte=today).count()
+
+    @property
+    def is_off_time(self, current_time=None) -> bool:
+        """
+        Check if the current time is within any off-time period for this page.
+        """
+        if not current_time:
+            current_time = timezone.localtime()
+
+        current_day = current_time.weekday()  # Monday is 0 and Sunday is 6
+        current_time_only = current_time.time()
+
+        # Filter off times for the current day
+        todays_off_times = self.off_times.filter(day_of_week=current_day)
+
+        for off_time in todays_off_times:
+            if off_time.start_time <= current_time_only <= off_time.end_time:
+                return True
+        return False
 
 
 class Report(BaseModel):
@@ -208,3 +228,28 @@ class DBLogEntry(models.Model):
     @property
     def short_message(self):
         return truncatechars(self.message, 50)
+
+
+class OffTime(models.Model):
+    DAYS_OF_WEEK = [
+        (0, "Monday"),
+        (1, "Tuesday"),
+        (2, "Wednesday"),
+        (3, "Thursday"),
+        (4, "Friday"),
+        (5, "Saturday"),
+        (6, "Sunday"),
+    ]
+
+    day_of_week = models.IntegerField(choices=DAYS_OF_WEEK)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    class Meta:
+        ordering = ["day_of_week", "start_time"]
+        verbose_name = "Off Time"
+        verbose_name_plural = "Off Times"
+
+    def __str__(self):
+        day = dict(self.DAYS_OF_WEEK).get(self.day_of_week, "Unknown")
+        return f"{day}: {self.start_time.strftime('%H:%M')} - {self.end_time.strftime('%H:%M')}"
